1da81c710d6a82f31abb35f7846c517704a41292
JoeRourke123 <josephrourke1@gmail.com>
2020-08-12 16:22:10 +0100

    Added some comments to my simple agent

--- src/simpleparty/SimpleParty.java
+++ src/simpleparty/SimpleParty.java
@@ -22,20 +22,26 @@ public class SimpleParty extends AbstractNegotiationParty {
         Action lastReceived = getLastReceivedAction();
         Bid lastBid;
 
-
+        // Checks that the agent has received an Offer and not some other form of message
         if(lastReceived instanceof Offer) {
             lastBid = ((Offer) lastReceived).getBid();
 
+            // Accept the bid if it's acceptable (i.e. has a high enough utility)
             if(getUtility(lastBid) >= UTIL_THRESHOLD) {
                 return new Accept(getPartyId(), lastBid);
             }
         }
+
+        // Otherwise, return a randomly generated bid
         Bid nextBid = generateRandomBid();
 
+        // Not sure this is the best method of doing so
+        // but this is a simple fix to only generate high utility bids which marginally improves bid utility
         while(getUtility(nextBid) < UTIL_THRESHOLD) {
             nextBid = generateRandomBid();
         }
 
+        // Send this offer to the other parties
         return new Offer(getPartyId(), nextBid);
     }
 
